33c61c48a3dd69b06740c17f573860b9e7e13ef5
aalfimov <steamcat089@gmail.com>
2019-03-20 20:27:47 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-const routes: Routes = [];
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {GarageComponent} from './garage/garage.component';
+import {CarComponent} from './car/car.component';
+import {CarDetailComponent} from './car-detail/car-detail.component';
+import {PageNotFoundComponentComponent} from './page-not-found-component/page-not-found-component.component';
+
+const routes: Routes = [
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'garage', component: GarageComponent},
+    {path: 'detail/:id', component: CarDetailComponent},
+    {path: 'cars', component: CarComponent},
+    {path: '**', component: PageNotFoundComponentComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
