c263136f5de6fa7faf70807462512a5f2ce1e41f
aalfimov <steamcat089@gmail.com>
2019-04-04 19:30:34 +0200

    Second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-const routes: Routes = [];
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {CarComponent} from './car/car.component';
+import {CarDetailComponent} from './car-detail/car-detail.component';
+import {PageNotFoundComponentComponent} from './page-not-found-component/page-not-found-component.component';
+
+const routes: Routes = [
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'detail/:id', component: CarDetailComponent},
+    {path: 'cars', component: CarComponent},
+    {path: '**', component: PageNotFoundComponentComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CarService} from './car.service';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'car-park-base';
+    title = 'Fleet Manager';
+    constructor(private carService: CarService) {
+    }
+    callCarsOnTheRoute() {
+        this.carService.parameterDashboard = true;
+    }
+
+    callCarsOnGarage() {
+        this.carService.parameterDashboard = false;
+    }
 }
